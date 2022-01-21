6115eff68da926444bc15a21c2233d9d16e0bcda
Ubuntu <ubuntu@ip-172-31-18-24.us-east-2.compute.internal>
2022-01-21 13:58:11 +0000

    additional stack

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class MyPipelineStack extends cdk.Stack {
 	  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,5 +14,28 @@ export class MyPipelineStack extends cdk.Stack {
 							              commands: ['npm ci', 'npm run build', 'npx cdk synth']
 								            })
 									        });
+										pipeline.addStage(new MyPipelineAppStage(this, "test", {
+											      env: { account: "294649033237", region: "us-east-2" }
+											          }));
+												  const testingStage = pipeline.addStage(new MyPipelineAppStage(this, 'testing', {
+													    env: { account: '294649033237', region: 'us-east-2' }
+												  }));
+
+												      testingStage.addPost(new ManualApprovalStep('approval'));
+												      // stage was returned by pipeline.addStage
+
+												      stage.addPost(new ShellStep("validate", {
+													        commands: ['curl -Ssf https://my.webservice.com/'],
+												      }));
+												      // given a stack lbStack that exposes a load balancer construct as loadBalancer
+												      this.loadBalancerAddress = new cdk.CfnOutput(lbStack, 'LbAddress', {
+													        value: `https://${lbStack.loadBalancer.loadBalancerDnsName}/`
+												      });
+
+														// pass the load balancer address to a shell step
+														stage.addPost(new ShellStep("lbaddr", {
+															  envFromCfnOutputs: {lb_addr: lbStack.loadBalancerAddress},
+															    commands: ['echo $lb_addr']
+														}));
 										  }
 }
